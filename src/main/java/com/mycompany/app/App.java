d4637f146353979056570f359d0b5d6c6edb7894
satyajitchampatiray <48091460+satyajitchampatiray@users.noreply.github.com>
2019-03-01 16:26:08 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World Satyajit!";
+    private final String message = "Hello World!";
 
     public App() {}
 
